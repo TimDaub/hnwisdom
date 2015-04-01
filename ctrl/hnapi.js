1e85254c22c5a75d91e6318a9336497ceef51a43
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2015-04-01 23:06:52 +0200

    heroku config vars

--- ctrl/hnapi.js
+++ ctrl/hnapi.js
@@ -1,8 +1,20 @@
 var request = require('request');
-var config = require('../config.json');
+var HNAPI;
+
+try {
+  var config = require('../config.json');
+} catch(e) {
+  console.log('config.json is not available, therefore I will use an environment variable.');
+}
+
+if(config && config.hnapi) {
+  HNAPI = config.hnapi;
+} else {
+  HNAPI = process.env.hnapi;
+}
 
 exports.getComment = function(hnid, next) {
-  request(config.hnapi + hnid + '.json', function (error, res, body) {
+  request(HNAPI  + hnid + '.json', function (error, res, body) {
     if (!error && res.statusCode == 200) {
       next(null, JSON.parse(body));
     } else {
